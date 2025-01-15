d1600eaa6949d6a7b12d0d1f2ea9f9a8cc4c345a
Sara Sundin <nbfux9sc@students.codeinstitute.net>
2025-01-15 10:23:34 +0000

    assign elements for shape picker

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Script loaded"); // Make DOM load before canvas
 });
     const features = {
+        defaultFace: {
+            "faceDefault":"assets/images/images_avatar/avatar_default.png",
+        }
         shape: {
             options: {
                 "female":"assets/images/images_avatar/shape_female.png",
@@ -12,3 +15,13 @@ document.addEventListener("DOMContentLoaded", () => {
             },
         },
     };
+
+    let SelectedFace = features.defaultFace.default; //Loading face 1st slide
+
+    // Canvas Setup
+const slide1 = document.getElementById("slide-1");
+const slide2 = document.getElementById("slide-2");
+const startButton = document.getElementById("start-button");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const shapePicker = document.getElementById("shape-picker");
