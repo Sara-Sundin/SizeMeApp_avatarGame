ab4e56b47eb950f3e0a9414c404ba6f874ce5dfc
Sara Sundin <nbfux9sc@students.codeinstitute.net>
2025-01-15 10:09:13 +0000

    add shape options js

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Avatar Generator
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Script loaded"); // Make DOM load before canvas
+});
+    const features = {
+        shape: {
+            options: {
+                "female":"assets/images/images_avatar/shape_female.png",
+                "male":"assets/images/images_avatar/shape_male.png",
+                "junior":"assets/images/images_avatar/shape_junior.png",
+            },
+        },
+    };
