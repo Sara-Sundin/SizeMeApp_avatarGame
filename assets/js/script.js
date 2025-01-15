3d40b26175090c0163e53011bceea5abe398033a
Sara Sundin <nbfux9sc@students.codeinstitute.net>
2025-01-15 10:50:10 +0000

    set up image default slide1

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Script loaded"); // Make DOM load before canvas
-});
+
     const features = {
         defaultFace: {
-            "faceDefault":"assets/images/images_avatar/avatar_default.png",
-        }
+            "defaultFace":"assets/images/images_avatar/avatar_default.png",
+        },
         shape: {
             options: {
                 "female":"assets/images/images_avatar/shape_female.png",
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
         },
     };
 
-    let SelectedFace = features.defaultFace.default; //Loading face 1st slide
+    let SelectedFace = features.defaultFace.defaultFace; //Loading face 1st slide
 
     // Canvas Setup
 const slide1 = document.getElementById("slide-1");
@@ -25,3 +25,24 @@ const startButton = document.getElementById("start-button");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const shapePicker = document.getElementById("shape-picker");
+
+// Canvas Dimensions
+canvas.width = 400;
+canvas.height = 400;
+
+function loadAndDrawImage(ImageSrc) {
+    const img = new Image();
+    img.src = ImageSrc;
+    img.onload = () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas before drawing
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    }
+}
+
+function setUpSlide1() {
+    loadAndDrawImage(features.defaultFace.defaultFace);
+}
+
+setUpSlide1();
+
+});
